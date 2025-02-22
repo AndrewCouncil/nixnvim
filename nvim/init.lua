d4ce839430b43daece4061398e978968ea43f1fa
drew-dirac <drew@diracinc.com>
2025-02-22 02:53:00 +0000

    added catppuccin theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,8 +4,8 @@ local opt = vim.o
 local g = vim.g
 
 -- <leader> key. Defaults to `\`. Some people prefer space.
--- g.mapleader = ' '
--- g.maplocalleader = ' '
+g.mapleader = ' '
+g.maplocalleader = ' '
 
 opt.compatible = false
 
@@ -41,6 +41,7 @@ opt.undofile = true
 opt.splitright = true
 opt.splitbelow = true
 opt.cmdheight = 0
+opt.termguicolors = true;
 
 opt.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
@@ -94,7 +95,7 @@ vim.diagnostic.config {
 
 g.editorconfig = true
 
-vim.opt.colorcolumn = '100'
+vim.cmd.colorscheme 'catppuccin'
 
 -- Native plugins
 cmd.filetype('plugin', 'indent', 'on')
